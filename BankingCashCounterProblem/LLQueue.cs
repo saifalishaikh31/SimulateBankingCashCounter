ff97840636bf1b73d0fc09f3578ab65c6e404605
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-16 23:18:06 +0530

    [Saif Ali] Add : Simulate Banking Cash Counter Added

---
+++ BankingCashCounterProblem/LLQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounterProblem
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    internal class LLQueue
+    {
+        internal Node head;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("\n{0} Customer Added to Queue", node.data);
+            Console.WriteLine(" ");
+        }
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty, Dequeue is not possible");
+                return;
+            }
+            while (head != null)
+            {
+                Node temp = head;
+                Console.WriteLine("\nCustomer Left the Queue : {0}", temp.data);
+                this.head = head.next;
+                temp = null;
+            }
+        }
+    }
+}
---
+++ BankingCashCounterProblem/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounterProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bank Cash Counter Problem");
+            LLQueue lLQueue = new LLQueue();
+            Console.WriteLine("People in Queue : ");
+            for (int i = 1; i < 5; i++)
+            {
+                lLQueue.Enqueue(i);
+                int totalBalance = 50000;
+                int depositAmt = 0;
+                int withdrawAmt = 0;
+                Console.WriteLine("1.Cash Withdraw"
+                               +"\n2.Cash Deposit"
+                               +"\n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("\nYour Total Account Balance : {0}",totalBalance);
+                        Console.Write("\nEnter Amount to Withdraw : ");
+                        withdrawAmt = Convert.ToInt32(Console.ReadLine());
+                        if(withdrawAmt > totalBalance)
+                        { 
+                            Console.WriteLine("\nYou have Insufficient Account Balance!!!"); 
+                        }
+                        else
+                        {
+                            totalBalance -= withdrawAmt;
+                            Console.WriteLine("\nPlease collect you Cash Money!!!");
+                            Console.WriteLine("\nYour Remaining Balance is : {0}",totalBalance);
+                        }
+                        lLQueue.Dequeue();
+                        break;
+                    case 2:
+                        Console.WriteLine("\nYour Total Account Balance : {0}", totalBalance);
+                        Console.Write("\nEnter Amount to Deposit : ");
+                        depositAmt = Convert.ToInt32(Console.ReadLine());
+                        totalBalance += depositAmt;
+                        Console.WriteLine("Your Total Account Balance : {0}",totalBalance);
+                        lLQueue.Dequeue();
+                        break;
+                    case 3:
+                        lLQueue.Dequeue();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option!!!");
+                        break;
+                }
+                Console.WriteLine("Thank You!!!");
+            }
+        }
+    }
+}
